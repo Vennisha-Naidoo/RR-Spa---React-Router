d250c51afdd84895e9796fa98da97ba92aef8281
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-04-08 23:57:12 +0200

    Used Link from React-Route-Dom to navigate to Product Page from Home Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-    return <h1> Welcome! </h1>
+    return <>
+        <h1> Welcome! </h1>
+        <p>Go To <Link to='/products'>Products</Link></p>
+    </>
 }
 
 export default HomePage;
