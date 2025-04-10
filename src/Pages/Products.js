38260ba46291d69d167682738a9e8f3d6c333182
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-04-10 23:20:46 +0200

    Used useNavigate by react-router-dom to programmatically navigate

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,5 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function ProductsPage() {
-    return <h1>Products</h1>
+
+    const navigate = useNavigate();
+
+    function navigateBackToHomePage() {
+        navigate('/');
+    }
+
+    return <>
+        <h1>Products</h1>
+        <button onClick={ navigateBackToHomePage }>Back To Home Page</button>
+    </>
 }
 
 export default ProductsPage;
