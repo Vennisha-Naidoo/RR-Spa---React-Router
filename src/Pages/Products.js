2f84eb2f70da86bb1b8f1d466bb720f28fb23612
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-04-10 23:32:22 +0200

    Defined and used dynamic routes (demo)

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -10,6 +10,11 @@ function ProductsPage() {
 
     return <>
         <h1>Products</h1>
+        <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>
+        </ul>
         <button onClick={ navigateBackToHomePage }>Back To Home Page</button>
     </>
 }
